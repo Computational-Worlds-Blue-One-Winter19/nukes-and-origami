6c33bdaa5fc5bede380b9681c99e67279572251d
Jim Rosales <rosales.jim@outlook.com>
2019-01-20 18:24:41 -0800

    Convert function use to classes where applicable

--- main.js
+++ main.js
@@ -1,134 +1,429 @@
+const AM = new AssetManager();
 
-const ASSET_MANAGER = new AssetManager();
+class Sprite {
+  constructor(source, originX, originY, frameWidth, frameHeight, numberOfFrames,
+    timePerFrame, scale, flip) {
+    this.sheet = source; // Source spritesheet
+    this.oriX = originX; // Top left X point of where to start on the spritesheet
+    this.oriY = originY; // Top left Y point of where to start on the spritesheet
+    this.width = frameWidth; // Pixel width of each frame
+    this.height = frameHeight; // Pixel height of each frame
+    this.len = numberOfFrames; // # of frames in this sprite (to let user pick
+    // certain frames from a sheet containing multiple animations)
+    this.time = timePerFrame; // time each frame should be displayed. If time = 0, don't loop
+    this.scale = scale; // Size scale
+    this.flip = flip; // BOOLEAN. Flip image over Y axis?
+    this.totalTime = timePerFrame * numberOfFrames; // Not set by user
+    this.elapsedTime = 0; // Not set by user
+    this.currentFrame = 0;
+  }
 
-function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames,
-  loop, reverse) {
-  this.spriteSheet = spriteSheet;
-  this.startX = startX;
-  this.startY = startY;
-  this.frameWidth = frameWidth;
-  this.frameDuration = frameDuration;
-  this.frameHeight = frameHeight;
-  this.frames = frames;
-  this.totalTime = frameDuration * frames;
-  this.elapsedTime = 0;
-  this.loop = loop;
-  this.reverse = reverse;
-}
 
-Animation.prototype.drawFrame = function drawFrame(tick, ctx, x, y, scaleBy) {
-  const localScaleBy = scaleBy || 1;
-  this.elapsedTime += tick;
-  if (this.loop) {
-    if (this.isDone()) {
-      this.elapsedTime = 0;
+  drawFrame(tick, ctx, x, y) {
+    this.elapsedTime += tick;
+    if (this.time !== 0) {
+      if (this.elapsedTime >= this.totalTime) { // The isDone() function does exactly this. Use either.
+      // All frames used. Start over to loop.
+        this.elapsedTime = 0;
+      }
+      this.currentFrame = (Math.floor(this.elapsedTime / this.time)) % this.len;
+    // console.log(this.currentFrame);
     }
-  } else if (this.isDone()) {
-    return;
-  }
-  let index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
-  let vindex = 0;
-  if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
-    index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
-    vindex += 1;
-  }
-  while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
-    index -= Math.floor(this.spriteSheet.width / this.frameWidth);
-    vindex += 1;
-  }
-
-  const locX = x;
-  const locY = y;
-  const offset = vindex === 0 ? this.startX : 0;
-  ctx.drawImage(this.spriteSheet,
-    index * this.frameWidth + offset, vindex * this.frameHeight + this.startY, // source from sheet
-    this.frameWidth, this.frameHeight,
-    locX, locY,
-    this.frameWidth * localScaleBy,
-    this.frameHeight * localScaleBy);
-};
-
-Animation.prototype.currentFrame = function currentFrame() {
-  return Math.floor(this.elapsedTime / this.frameDuration);
-};
-
-Animation.prototype.isDone = function isDone() {
-  return (this.elapsedTime >= this.totalTime);
-};
-
-function Background(game) {
-  Entity.call(this, game, 0, 400);
-  this.radius = 200;
-}
-
-Background.prototype = new Entity();
-Background.prototype.constructor = Background;
-
-Background.prototype.update = function update() {
-};
-
-Background.prototype.draw = function draw(ctx) {
-  ctx.fillStyle = 'SaddleBrown';
-  ctx.fillRect(0, 500, 800, 300);
-  Entity.prototype.draw.call(this);
-};
-
-function Unicorn(game) {
-  this.animation = new Animation(ASSET_MANAGER.getAsset('./img/RobotUnicorn.png'), 0, 0, 206, 110, 0.02, 30, true, true);
-  this.jumpAnimation = new Animation(ASSET_MANAGER.getAsset('./img/RobotUnicorn.png'), 618, 334, 174, 138, 0.02, 40, false, true);
-  this.jumping = false;
-  this.radius = 100;
-  this.ground = 400;
-  Entity.call(this, game, 0, 400);
-}
-
-Unicorn.prototype = new Entity();
-Unicorn.prototype.constructor = Unicorn;
-
-Unicorn.prototype.update = function update() {
-  if (this.game.space) this.jumping = true;
-  if (this.jumping) {
-    if (this.jumpAnimation.isDone()) {
-      this.jumpAnimation.elapsedTime = 0;
-      this.jumping = false;
+    if (this.flip) {
+      ctx.save();
+      ctx.scale(-1, 1);
+      ctx.drawImage(this.sheet,
+        this.oriX + (this.width * this.currentFrame),
+        this.oriY, // NOTE: does not work for spritesheets where one animation goes to next line!
+        this.width,
+        this.height,
+        x * -1, // Flip
+        y,
+        this.width * this.scale,
+        this.height * this.scale);
+      ctx.restore();
+    } else {
+    // console.log("draw");
+    // console.log("cutting out X:" + (this.oriX + (this.width * this.currentFrame)) + ", Y: " + this.oriY);
+      ctx.drawImage(this.sheet,
+        this.oriX + (this.width * this.currentFrame),
+        this.oriY, // NOTE: does not work for spritesheets where one animation goes to next line!
+        this.width,
+        this.height,
+        x,
+        y,
+        this.width * this.scale,
+        this.height * this.scale);
     }
-    let jumpDistance = this.jumpAnimation.elapsedTime / this.jumpAnimation.totalTime;
-    const totalHeight = 200;
-
-    if (jumpDistance > 0.5) { jumpDistance = 1 - jumpDistance; }
-
-    // var height = jumpDistance * 2 * totalHeight;
-    const height = totalHeight * (-4 * (jumpDistance * jumpDistance - jumpDistance));
-    this.y = this.ground - height;
+    if (this.time !== 0) {
+      this.currentFrame += 1;
+    }
   }
-  Entity.prototype.update.call(this);
-};
 
-Unicorn.prototype.draw = function draw(ctx) {
-  if (this.jumping) {
-    this.jumpAnimation.drawFrame(this.game.clockTick, ctx, this.x + 17, this.y - 34);
-  } else {
-    this.animation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+  isDone() {
+    return this.elapsedTime >= this.totalTime;
   }
-  Entity.prototype.draw.call(this);
-};
+}
 
-// the "main" code begins here
+class Background extends Entity {
+  constructor(game, spritesheet) {
+    super(game, 0, 0);
+    this.x = 0;
+    this.y = 0;
+    this.spritesheet = spritesheet;
+    this.game = game;
+    this.ctx = game.ctx;
+  }
 
-ASSET_MANAGER.queueDownload('./img/RobotUnicorn.png');
+  draw() {
+    this.ctx.drawImage(this.spritesheet,
+      this.x, this.y);
+  }
 
-ASSET_MANAGER.downloadAll(() => {
-  console.log('starting up da sheild');
+  // update() {
+  // };
+}
+
+
+class Slippy extends Entity {
+  constructor(game, spritesheet) {
+    super(game, 200, 700);
+    this.x = 200;
+    this.y = 700;
+
+    this.start = spritesheet[0];
+    this.roll = spritesheet[1];
+    this.greatJob = spritesheet[2];
+    this.missionDone = spritesheet[3];
+    this.end = spritesheet[4];
+    // this.spritesheet = this.start;
+    this.game = game;
+    this.ctx = game.ctx;
+    this.byeslippy = this.ctx;
+  }
+
+  draw() {
+    if (this.game.timer.gameTime >= 1 && this.game.timer.gameTime <= 3) {
+      this.ctx.drawImage(this.start, this.x, this.y, 1000, 350);
+    } else if (this.game.timer.gameTime >= 6.5 && this.game.timer.gameTime <= 8) {
+      this.ctx.drawImage(this.roll, this.x, this.y, 1000, 350);
+    } else if (this.game.timer.gameTime > 10 && this.game.timer.gameTime <= 12) {
+      this.ctx.drawImage(this.greatJob, this.x, this.y, 1000, 350);
+    } else if (this.game.timer.gameTime > 14 && this.game.timer.gameTime < 16) {
+      this.ctx.drawImage(this.missionDone, this.x, this.y, 1000, 350);
+    } else if (this.game.timer.gameTime > 16.2 && this.game.timer.gameTime <= 18.5) {
+      this.ctx.drawImage(this.end, this.x, this.y, 1000, 350);
+    }
+  }
+
+  // update() {
+  // console.log(this.game.timer.gameTime);
+  // if(this.game.timer.gameTime >= 10) {
+  // console.log("call");
+  // this.draw();
+  // }
+  // }
+}
+
+
+class Nuke extends Entity {
+  constructor(game, spritesheet) {
+    super(game, 850, -25);
+    this.sprite = new Sprite(spritesheet, 0, 0, 320, 232, 25, 0.02, 1, true);
+    this.game = game;
+    this.ctx = game.ctx;
+    this.done = false;
+    // Entity.call(game, 850, -25);
+  }
+
+  // Nuke.prototype = new Entity();
+  // Nuke.prototype.constructor = Nuke;
+
+  draw() {
+    if (this.game.timer.gameTime >= 12.3 && !this.done) {
+      this.sprite.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+      if (this.sprite.currentFrame === this.sprite.len) {
+        this.done = true;
+      }
+    }
+  }
+
+  // update() {
+
+  // }
+}
+
+
+// (source, originX, originY, frameWidth, frameHeight, numberOfFrames, timePerFrame, scale, flip)
+class Crane extends Entity {
+  constructor(game, spritesheet) {
+    super(game, 550, -110);
+    this.sprite = new Sprite(spritesheet, 0, 0, 440, 330, 4, 0.1, 0.7, false);
+    this.originalScale = 0.7;
+    // 0.7 scaled x = 550
+    this.speed = 50;
+    this.game = game;
+    this.ctx = game.ctx;
+    this.isIdle = false;
+    this.idleTrans = false;
+    this.idleCount = 0;
+    this.alive = true; // y = 550
+    // this.Entity = new Entity();
+    // this.Entity.apply(this, game, 550, -110);
+    // Entity.call(this, game, 550, -110);
+    // x += ((x * oldscale - newscale) / 2)
+    // y += ((y * oldscale - newscale) / 2)
+  }
+
+
+  draw() {
+    if (this.alive) { this.sprite.drawFrame(this.game.clockTick, this.ctx, this.x, this.y); }
+  }
+
+  update() {
+    if (this.y <= 0) {
+      this.y += this.speed * this.game.clockTick;
+    } else {
+      this.isIdle = true;
+    }
+    if (this.game.timer.gameTime > 12.4) {
+      this.alive = false;
+    }
+    // Idle hover effect
+    if (this.isIdle) {
+      if (this.idleTrans) {
+        this.idleCount += 1;
+        // TODO: you can make this simpler
+        if (this.idleCount % 30 === 0) {
+          this.y += 1;
+        }
+        if (this.idleCount === 300) {
+          this.idleTrans = !this.idleTrans;
+          this.idleCount = 0;
+        }
+      } else {
+        this.idleCount += 1;
+        if (this.idleCount % 30 === 0) {
+          this.y -= 1;
+        }
+        if (this.idleCount === 300) {
+          this.idleTrans = !this.idleTrans;
+          this.idleCount = 0;
+        }
+      }
+    }
+    // console.log(`update crane: ${this.x}`);
+  }
+}
+
+// Crane.prototype = new Entity();
+// Crane.prototype.constructor = Crane;
+
+
+class Plane extends Entity {
+  constructor(game, spritesheet) {
+    super(game, 650, 1500);
+    this.idle = new Sprite(spritesheet, 0, 0, 300, 330, 1, 0, 0.4, false);
+    this.right = new Sprite(spritesheet, 300, 0, 300, 330, 1, 0, 0.4, false);
+    this.left = new Sprite(spritesheet, 600, 0, 300, 330, 1, 0, 0.4, false);
+    this.rollRight = new Sprite(spritesheet, 0, 330, 300, 330, 8, 0.15, 0.4, false);
+    this.rollLeft = new Sprite(spritesheet, 0, 660, 300, 330, 8, 0.15, 0.4, false);
+    this.sprite = this.idle;
+    this.speed = 500;
+    this.turn = false;
+    this.game = game;
+    this.ctx = game.ctx;
+    this.isIdle = false;
+    this.idleTrans = false;
+    this.idleCount = 0;
+    // Entity.call(this, game, 650, 1500);
+  }
+
+  // Plane.prototype = new Entity();
+  // Plane.prototype.constructor = Plane;
+
+  update() {
+    if (this.y >= 600) {
+      this.y -= this.speed * this.game.clockTick;
+      this.isIdle = true;
+    }
+    if (this.game.timer.gameTime > 7 && this.game.timer.gameTime < 8) {
+      this.isIdle = false;
+      this.sprite = this.rollLeft;
+      if (this.sprite.currentFrame > 0) {
+        this.x -= 350 * this.game.clockTick;
+      }
+    }
+    if (this.game.timer.gameTime > 8 && this.sprite.isDone) {
+      this.isIdle = true;
+      this.sprite = this.idle;
+    }
+    if (this.game.timer.gameTime > 9 && this.x <= 650) {
+      this.isIdle = false;
+      this.sprite = this.right;
+      this.x += this.speed * this.game.clockTick;
+    }
+    if (this.game.timer.gameTime > 18 && this.game.timer.gameTime < 20) {
+      this.sprite = this.right;
+      this.isIdle = false;
+      this.x += this.speed * this.game.clockTick;
+      // console.log(this.x);
+      this.y -= this.speed * this.game.clockTick;
+    }
+    if (this.game.timer.gameTime > 20) {
+      this.x = 650;
+      this.y = 1500;
+      this.game.timer.gameTime = 0;
+    }
+    // Idle hover effect
+    if (this.isIdle) {
+      if (this.idleTrans) {
+        this.idleCount += 1;
+        // every 10 frames
+        if (this.idleCount % 10 === 0) {
+          this.y += 1;
+        }
+        // go other way every 50 frames
+        if (this.idleCount === 50) {
+          this.idleTrans = !this.idleTrans;
+          this.idleCount = 0;
+        }
+      } else {
+        this.idleCount += 1;
+        if (this.idleCount % 10 === 0) {
+          this.y -= 1;
+        }
+        if (this.idleCount === 50) {
+          this.idleTrans = !this.idleTrans;
+          this.idleCount = 0;
+        }
+      }
+    }
+  }
+
+  draw() {
+    this.sprite.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+  }
+}
+
+// (source, originX, originY, frameWidth, frameHeight, numberOfFrames, timePerFrame, scale, flip)
+class Bullet extends Entity {
+  constructor(game, spritesheet) {
+    super(game, -50, -50);
+    this.sprite = new Sprite(spritesheet, 0, 0, 640, 320, 1, 0, 0.04, false);
+    this.game = game;
+    this.speed = 500;
+    this.angleLeftX1 = 690;
+    this.angleLeftX2 = 690;
+    this.angleRightX1 = 690;
+    this.angleRightX2 = 690;
+    this.ctx = game.ctx;
+    this.spawned = false;
+    this.repeated = false;
+    this.craneshot = false;
+    this.planeshot = false;
+    // Entity.call(this, game, -50, -50); // start off screen
+    // 685, 580 = right in front of plane.
+  }
+
+  update() {
+    if (this.game.timer.gameTime > 7 && this.game.timer.gameTime < 7.1) {
+      this.x = 690;
+      this.y = 200;
+      this.spawned = true;
+      this.craneshot = true;
+    }
+    if ((this.spawned && this.craneshot) || (this.repeated && this.craneshot)) {
+      // this.y -= this.speed * this.game.clockTick; Bullet from plane
+      this.y += this.speed * this.game.clockTick;
+      this.angleLeftX1 -= this.speed * this.game.clockTick;
+      this.angleLeftX2 -= this.speed / 2 * this.game.clockTick;
+      this.angleRightX1 += this.speed * this.game.clockTick;
+      this.angleRightX2 += this.speed / 2 * this.game.clockTick;
+    }
+    if (this.y > 700 && !this.repeated && this.craneshot) {
+      this.repeated = true;
+      this.x = 690;
+      this.y = 200;
+      this.angleLeftX1 = 690;
+      this.angleLeftX2 = 690;
+      this.angleRightX1 = 690;
+      this.angleRightX2 = 690;
+    }
+    // if(this.repeated  && this.craneshot)   {
+    // this.y += this.speed * this.game.clockTick;
+    // }
+    if (this.repeated && this.y > 700 && this.craneshot) {
+      // hide bullet
+      this.x = -500;
+      this.y = -500;
+      this.repeated = false;
+      this.craneshot = false;
+    }
+    if (this.game.timer.gameTime > 11.5 && this.game.timer.gameTime < 11.6) {
+      this.planeshot = true;
+      this.x = 690;
+      this.y = 585;
+    }
+    if (this.planeshot) {
+      this.y -= this.speed * this.game.clockTick;
+    }
+    if (this.planeshot && this.y <= 200) {
+      this.planeshot = false;
+      console.log(this.game.timer.gameTime);
+      this.x = -500;
+      this.y = -500;
+    }
+  }
+
+
+  draw() {
+    this.sprite.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    if (this.craneshot) {
+      this.sprite.drawFrame(this.game.clockTick, this.ctx, this.angleLeftX1, this.y);
+      this.sprite.drawFrame(this.game.clockTick, this.ctx, this.angleLeftX2, this.y);
+      this.sprite.drawFrame(this.game.clockTick, this.ctx, this.angleRightX1, this.y);
+      this.sprite.drawFrame(this.game.clockTick, this.ctx, this.angleRightX2, this.y);
+    }
+  }
+}
+
+// Bullet.prototype = new Entity();
+// Bullet.prototype.constructor = Bullet;
+
+AM.queueDownload('./img/crane-sheet.png');
+AM.queueDownload('./img/plane.png');
+AM.queueDownload('./img/spacebg.png');
+AM.queueDownload('./img/paper.png');
+AM.queueDownload('./img/bullet.png');
+AM.queueDownload('./img/slippy_roll.png');
+AM.queueDownload('./img/slippy_end.png');
+AM.queueDownload('./img/slippy_greatjob.png');
+AM.queueDownload('./img/slippy_inbound.png');
+AM.queueDownload('./img/slippy_mission_done.png');
+AM.queueDownload('./img/nuke_single.png');
+
+AM.downloadAll(() => {
   const canvas = document.getElementById('gameWorld');
   const ctx = canvas.getContext('2d');
 
   const gameEngine = new GameEngine();
-  const bg = new Background(gameEngine);
-  const unicorn = new Unicorn(gameEngine);
-
-  gameEngine.addEntity(bg);
-  gameEngine.addEntity(unicorn);
-
   gameEngine.init(ctx);
   gameEngine.start();
+
+  const slippyArr = [AM.getAsset('./img/slippy_inbound.png'),
+    AM.getAsset('./img/slippy_roll.png'),
+    AM.getAsset('./img/slippy_greatjob.png'),
+    AM.getAsset('./img/slippy_mission_done.png'),
+    AM.getAsset('./img/slippy_end.png')];
+
+  gameEngine.addEntity(new Background(gameEngine, AM.getAsset('./img/spacebg.png')));
+  // gameEngine.addEntity(new Background(gameEngine, AM.getAsset('./img/paper.png')));
+  gameEngine.addEntity(new Crane(gameEngine, AM.getAsset('./img/crane-sheet.png')));
+  gameEngine.addEntity(new Plane(gameEngine, AM.getAsset('./img/plane.png')));
+  gameEngine.addEntity(new Slippy(gameEngine, slippyArr));
+  gameEngine.addEntity(new Nuke(gameEngine, AM.getAsset('./img/nuke_single.png')));
+  gameEngine.addEntity(new Bullet(gameEngine, AM.getAsset('./img/bullet.png')));
+
+  console.log('All Done!');
 });
