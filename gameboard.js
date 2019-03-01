5f0a12f25f85299232ad69ce3be56cc89662a1fc
Jim Rosales <rosales.jim@outlook.com>
2019-02-28 21:37:10 -0800

    Testing new audio engine

--- gameboard.js
+++ gameboard.js
@@ -201,6 +201,162 @@ function hideControlMessage() {
   message.style.display = 'none';
 }
 
+//--------------
+    // Audio Object
+    //--------------
+    var audio = {
+      buffer: {},
+      compatibility: {},
+      files: [
+          'https://forestmist.org/share/web-audio-api-demo/audio/beat.wav',
+          // '/audio/Game_Loop_v.1.ogg',
+          // 'beat.wav'
+      ],
+      proceed: true,
+      source_loop: {},
+      source_once: {}
+  };
+
+  //-----------------
+  // Audio Functions
+  //-----------------
+  audio.findSync = function(n) {
+      var first = 0,
+          current = 0,
+          offset = 0;
+
+      // Find the audio source with the earliest startTime to sync all others to
+      for (var i in audio.source_loop) {
+          current = audio.source_loop[i]._startTime;
+          if (current > 0) {
+              if (current < first || first === 0) {
+                  first = current;
+              }
+          }
+      }
+
+      if (audio.context.currentTime > first) {
+          offset = (audio.context.currentTime - first) % audio.buffer[n].duration;
+      }
+
+      return offset;
+  };
+
+  audio.play = function(n) {
+      if (audio.source_loop[n]._playing) {
+          audio.stop(n);
+      } else {
+          audio.source_loop[n] = audio.context.createBufferSource();
+          audio.source_loop[n].buffer = audio.buffer[n];
+          audio.source_loop[n].loop = true;
+          audio.source_loop[n].connect(audio.context.destination);
+
+          var offset = audio.findSync(n);
+          audio.source_loop[n]._startTime = audio.context.currentTime;
+
+          if (audio.compatibility.start === 'noteOn') {
+              /*
+              The depreciated noteOn() function does not support offsets.
+              Compensate by using noteGrainOn() with an offset to play once and then schedule a noteOn() call to loop after that.
+              */
+              audio.source_once[n] = audio.context.createBufferSource();
+              audio.source_once[n].buffer = audio.buffer[n];
+              audio.source_once[n].connect(audio.context.destination);
+              audio.source_once[n].noteGrainOn(0, offset, audio.buffer[n].duration - offset); // currentTime, offset, duration
+              /*
+              Note about the third parameter of noteGrainOn().
+              If your sound is 10 seconds long, your offset 5 and duration 5 then you'll get what you expect.
+              If your sound is 10 seconds long, your offset 5 and duration 10 then the sound will play from the start instead of the offset.
+              */
+
+              // Now queue up our looping sound to start immediatly after the source_once audio plays.
+              audio.source_loop[n][audio.compatibility.start](audio.context.currentTime + (audio.buffer[n].duration - offset));
+          } else {
+              audio.source_loop[n][audio.compatibility.start](0, offset);
+          }
+
+          audio.source_loop[n]._playing = true;
+      }
+  };
+
+  audio.stop = function(n) {
+      if (audio.source_loop[n]._playing) {
+          audio.source_loop[n][audio.compatibility.stop](0);
+          audio.source_loop[n]._playing = false;
+          audio.source_loop[n]._startTime = 0;
+          if (audio.compatibility.start === 'noteOn') {
+              audio.source_once[n][audio.compatibility.stop](0);
+          }
+      }
+  };
+
+  //-----------------------------
+  // Check Web Audio API Support
+  //-----------------------------
+  try {
+      // More info at http://caniuse.com/#feat=audio-api
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      audio.context = new window.AudioContext();
+  } catch(e) {
+      audio.proceed = false;
+      alert('Web Audio API not supported in this browser.');
+  }
+
+  if (audio.proceed) {
+      //---------------
+      // Compatibility
+      //---------------
+      (function() {
+          var start = 'start',
+              stop = 'stop',
+              buffer = audio.context.createBufferSource();
+
+          if (typeof buffer.start !== 'function') {
+              start = 'noteOn';
+          }
+          audio.compatibility.start = start;
+
+          if (typeof buffer.stop !== 'function') {
+              stop = 'noteOff';
+          }
+          audio.compatibility.stop = stop;
+      })();
+
+      //-------------------------------
+      // Setup Audio Files and Buttons
+      //-------------------------------
+      for (var a in audio.files) {
+          (function() {
+              var i = parseInt(a) + 1;
+              var req = new XMLHttpRequest();
+              req.open('GET', audio.files[i - 1], true); // array starts with 0 hence the -1
+              // req.withCredentials = true;
+              req.responseType = 'arraybuffer';
+              console.log("Making the requests");
+              req.onload = function() {
+                  audio.context.decodeAudioData(
+                      req.response,
+                      function(buffer) {
+                          audio.buffer[i] = buffer;
+                          audio.source_loop[i] = {};
+                          audio.source_loop[i]._playing = false
+                          console.log("Got a response");
+                          // var button = document.getElementById('button-loop-' + i);
+                          // button.addEventListener('click', function(e) {
+                          //     e.preventDefault();
+                          //     audio.play(this.value);
+                          // });
+                      },
+                      function() {
+                          console.log('Error decoding audio "' + audio.files[i - 1] + '".');
+                      }
+                  );
+              };
+              req.send();
+          })();
+      }
+  }
+
 /**
  * Starts the game by spawning enemies, initializing the scoreboard lives and
  * setting the focus to the canvas
@@ -208,18 +364,18 @@ function hideControlMessage() {
  */
 function startGame(game) {
   game.initializeSceneManager();
+  audio.play(1);
+//   // playLoop(game.sounds.gameLoop);
+// let loop = new SeamlessLoop();
+// loop.addUri("audio/Game_Loop_v.1.ogg", 9590, 'sound1');
+// loop._volume = 0.09;
+// loop.callback(soundsLoaded);
 
-  // playLoop(game.sounds.gameLoop);
-let loop = new SeamlessLoop();
-loop.addUri("audio/Game_Loop_v.1.ogg", 9590, 'sound1');
-loop._volume = 0.09;
-loop.callback(soundsLoaded);
-
-function soundsLoaded() {
-    let n = 1;
-    loop._volume = 0.09;
-    loop.start('sound' + n);
-}
+// function soundsLoaded() {
+//     let n = 1;
+//     loop._volume = 0.09;
+//     loop.start('sound' + n);
+// }
 
 
 
