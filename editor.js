e419bd84c8c2322f70d56ace66b100f1ddb02cc2
rueschenberg <nathanfms@gmail.com>
2019-02-28 18:16:23 -0800

    editor files

---
+++ editor.js
@@ -0,0 +1,30 @@
+class Editor {
+    constructor(game)   {
+        this.game = game;
+        this.ctx = game.ctx;
+        this.game.editorPause();
+        this.ctx.clearRect(0, 0, 2000, 2000);    
+        this.init();
+    }
+
+    init()  {
+        addEvent(
+            document.getElementById('goBack'),
+            'click',
+            () => {
+              const container = document.getElementById('scoreboard');
+              container.style.display = 'initial';
+              const editor = document.getElementById('editor');
+              editor.style.display = 'none';
+              const fps = document.getElementById('fps')
+              fps.style.display = 'initial';
+              this.game.resume();
+            },
+        );
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.dropdown-trigger');
+            var instances = M.Dropdown.init(elems, options);
+          });
+
+    }
+}
