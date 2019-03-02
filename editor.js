2ecef5b0aa71ef2f1b6cf117d9859d887821cd47
rueschenberg <nathanfms@gmail.com>
2019-03-01 16:07:51 -0800

    changed dropdowns to apply automatically instead of using canvas onclick

--- editor.js
+++ editor.js
@@ -15,7 +15,7 @@ class Editor {
     }
 
     init()  {
-
+        const options = {};
         this.previewCanvas = document.getElementById('preview');
         this.preview = this.previewCanvas.getContext('2d');
         this.previewGame.init(this.preview);
@@ -35,66 +35,55 @@ class Editor {
               this.originalGame.resume();
             },
         );
-        // var elems = document.querySelectorAll('.dropdown-trigger');
-        const options = {};
-        // var instances = M.Dropdown.init(elems, options);
-        //var elems = document.querySelectorAll('.dropdown-trigger');
         var elems = document.querySelectorAll('select');
         this.instances = M.FormSelect.init(elems, options);
-        //const val = M.Dropdown.getInstance(dropDown);
-        addEvent(
-            document.getElementById('preview'),
-            'click',
-            () => {
-                this.previewGame.clearEntities();
-                let manifest = this.getEnemySelection();
-                let add = new Ship(this.previewGame, manifest);
-                // add.sprite.scale -= 0.2;
-                add.current.x = 100;
-                add.current.y = 0;
-                if(this.instances[2].el.value != 'None')    {
-                    add.initializeWeapon(eval(this.instances[2].el.value));
-                }
-                this.previewGame.addEntity(add);
-                this.game.sceneManager.loadBackground(eval(this.instances[0].el.value));
-                //this.previewGame.addEntity(getEnemySelection());
-                // console.log(val.getSelectedValues());
-                var div = document.createElement('div');
-                div.setAttribute('class', 'timelineElement');
-                div.style.marginLeft = '105px';
-                // div.style.marginTop = '-100px';
-                document.getElementById('timelineBorder').appendChild(div);
-            },
-        );
+        var gameReference = this.game;
+        var previewReference = this.previewGame;
+        document.addEventListener('change', function(event) {
+            switch(event.target.id) {
+                case 'chooseBackground':
+                    gameReference.sceneManager.loadBackground(eval(event.target.value));
+                    break;
+                case 'chooseEnemy':
+                    previewReference.clearEntities();
+                    let add = new Ship(previewReference, eval(event.target.value));
+                    add.current.x = 100;
+                    add.current.y = 0;
+                    previewReference.addEntity(add);
+                    break;
+                case 'chooseWeapon':
+                    previewReference.ship.initializeWeapon(eval(event.target.value));
+                    break;
+                case 'choosePath':
+                    console.log("Path chosen: " + event.target.value);
+                    break;
+            }
+        });
+        // addEvent(
+        //     document.getElementById('preview'),
+        //     'click',
+        //     () => {
+        //         this.previewGame.clearEntities();
+        //         let manifest = this.getEnemySelection();
+        //         let add = new Ship(this.previewGame, manifest);
+        //         // add.sprite.scale -= 0.2;
+        //         add.current.x = 100;
+        //         add.current.y = 0;
+        //         if(this.instances[2].el.value != 'None')    {
+        //             add.initializeWeapon(eval(this.instances[2].el.value));
+        //         }
+        //         this.previewGame.addEntity(add);
+        //         this.game.sceneManager.loadBackground(eval(this.instances[0].el.value));
+        //         //this.previewGame.addEntity(getEnemySelection());
+        //         // console.log(val.getSelectedValues());
+        //         var div = document.createElement('div');
+        //         div.setAttribute('class', 'timelineElement');
+        //         div.style.marginLeft = '105px';
+        //         // div.style.marginTop = '-100px';
+        //         document.getElementById('timelineBorder').appendChild(div);
+        //     },
+        // );
         document.querySelectorAll('#fps').forEach(e => e.style.display = 'none');
-        //console.log(fps);
-    }
-
-    getEnemySelection() { //TODO: REMOVE ME + REFACTOR HTML AND USE eval() INSTEAD!!
-        switch(this.instances[1].el.value)  {
-            case 'Bat':
-                return ship.bat;
-            case 'Bird':
-                return ship.bird;
-            case 'Crane':
-                return ship.crane;
-            case 'Dove':
-                return ship.dove;
-            case 'Eagle':
-                return ship.previewEagle;
-            case 'Goose':
-                return ship.goose;
-            case 'Hummer':
-                return ship.hummer;
-            case 'Owl':
-                return ship.owl;
-            case 'Pigeon':
-                return ship.pigeon;
-            case 'Swallow':
-                return ship.swallow;
-            default:
-                return null;
-        }
     }
 
 }
@@ -165,6 +154,18 @@ class Scene {
 }
 
 class Preview extends GameEngine    {
+    constructor()   {
+        super();
+        this.ship;
+    }
+
+    addEntity(e)    {
+        super.addEntity(e);
+        if(e instanceof Ship)   {
+            this.ship = e;
+        }
+    }
+
     getPlayerLocation(point) {
         const deltaX = 0;
         const deltaY = 300;
